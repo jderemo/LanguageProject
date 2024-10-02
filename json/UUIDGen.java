921e5fe9a5e92c9c15251111fd0ee97e432dc4ba
sockrubbah <solomander81@gmail.com>
2024-10-02 10:22:22 -0400

    Json

--- json/UUIDGen.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.UUID;
-
-public class UUIDGen {
-    public static void main(String[] args) {
-        UUID randID = UUID.randomUUID();
-        System.out.println(randID.toString());
-    }
-}
