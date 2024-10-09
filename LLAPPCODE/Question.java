fa3c8313b00e8306db291ea5ade05da14dcd5cff
sockrubbah <solomander81@gmail.com>
2024-10-09 17:10:53 -0400

    adding coding files

---
+++ LLAPPCODE/Question.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Question {
+
+    private static Map<String, String> wordBank = new HashMap<>();
+
+    public static Map<String, String> getWordBank() {
+        return wordBank;
+    }
+
+    public static void expandWordBank(String englishWord, String translatedWord) {
+        wordBank.put(englishWord, translatedWord);
+    }
+
+    public abstract void startExercise();
+
+    public abstract boolean checkAnswer(String userAnswer);
+
+    public abstract String getQuestion();
+
+    public abstract String getAnswer();
+
+    public abstract String getExerciseID();
+    
+}
