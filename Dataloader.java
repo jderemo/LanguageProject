f2811dffaf5bf97e3d02e4dba100a01afa87d82f
jderemo <jeffreyderemoo@gmail.com>
2024-10-10 09:15:48 -0400

    dd\id s

---
+++ Dataloader.java
@@ -0,0 +1,18 @@
+public class Dataloader {
+
+    
+} 
+    public static boolean saveUsers(){
+
+    }
+    public static boolean saveProjects() {
+    
+    }
+    public static JSONObject getProjectJSON(Project project){
+
+    }
+
+    public static JSONObject getColumnJSON(Column column) {
+    }
+
+    
